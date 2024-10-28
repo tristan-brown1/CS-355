abfc7bc27b2056089ba20f385679bb23daa928cf
Tristan Brown <2tristanbrown@gmail.com>
2024-10-28 18:12:38 -0400

    updates to lab 7

--- Lab7/Lab7.py
+++ Lab7/Lab7.py
@@ -1,77 +1,79 @@
 # Import a library of functions called 'pygame'
+import math
+
 import pygame
 from math import pi
 
 class Point:
-	def __init__(self,x,y):
-		self.x = x
-		self.y = y
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
 
 class Point3D:
-	def __init__(self,x,y,z):
-		self.x = x
-		self.y = y
-		self.z = z
-		
+    def __init__(self,x,y,z):
+        self.x = x
+        self.y = y
+        self.z = z
+
 class Line3D():
-	
-	def __init__(self, start, end):
-		self.start = start
-		self.end = end
+
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
 
 def loadOBJ(filename):
-	
-	vertices = []
-	indices = []
-	lines = []
-	
-	f = open(filename, "r")
-	for line in f:
-		t = str.split(line)
-		if not t:
-			continue
-		if t[0] == "v":
-			vertices.append(Point3D(float(t[1]),float(t[2]),float(t[3])))
-			
-		if t[0] == "f":
-			for i in range(1,len(t) - 1):
-				index1 = int(str.split(t[i],"/")[0])
-				index2 = int(str.split(t[i+1],"/")[0])
-				indices.append((index1,index2))
-			
-	f.close()
-	
-	#Add faces as lines
-	for index_pair in indices:
-		index1 = index_pair[0]
-		index2 = index_pair[1]
-		lines.append(Line3D(vertices[index1 - 1],vertices[index2 - 1]))
-		
-	#Find duplicates
-	duplicates = []
-	for i in range(len(lines)):
-		for j in range(i+1, len(lines)):
-			line1 = lines[i]
-			line2 = lines[j]
-			
-			# Case 1 -> Starts match
-			if line1.start.x == line2.start.x and line1.start.y == line2.start.y and line1.start.z == line2.start.z:
-				if line1.end.x == line2.end.x and line1.end.y == line2.end.y and line1.end.z == line2.end.z:
-					duplicates.append(j)
-			# Case 2 -> Start matches end
-			if line1.start.x == line2.end.x and line1.start.y == line2.end.y and line1.start.z == line2.end.z:
-				if line1.end.x == line2.start.x and line1.end.y == line2.start.y and line1.end.z == line2.start.z:
-					duplicates.append(j)
-					
-	duplicates = list(set(duplicates))
-	duplicates.sort()
-	duplicates = duplicates[::-1]
-	
-	#Remove duplicates
-	for j in range(len(duplicates)):
-		del lines[duplicates[j]]
-	
-	return lines
+
+    vertices = []
+    indices = []
+    lines = []
+
+    f = open(filename, "r")
+    for line in f:
+        t = str.split(line)
+        if not t:
+            continue
+        if t[0] == "v":
+            vertices.append(Point3D(float(t[1]),float(t[2]),float(t[3])))
+
+        if t[0] == "f":
+            for i in range(1,len(t) - 1):
+                index1 = int(str.split(t[i],"/")[0])
+                index2 = int(str.split(t[i+1],"/")[0])
+                indices.append((index1,index2))
+
+    f.close()
+
+    #Add faces as lines
+    for index_pair in indices:
+        index1 = index_pair[0]
+        index2 = index_pair[1]
+        lines.append(Line3D(vertices[index1 - 1],vertices[index2 - 1]))
+
+    #Find duplicates
+    duplicates = []
+    for i in range(len(lines)):
+        for j in range(i+1, len(lines)):
+            line1 = lines[i]
+            line2 = lines[j]
+
+            # Case 1 -> Starts match
+            if line1.start.x == line2.start.x and line1.start.y == line2.start.y and line1.start.z == line2.start.z:
+                if line1.end.x == line2.end.x and line1.end.y == line2.end.y and line1.end.z == line2.end.z:
+                    duplicates.append(j)
+            # Case 2 -> Start matches end
+            if line1.start.x == line2.end.x and line1.start.y == line2.end.y and line1.start.z == line2.end.z:
+                if line1.end.x == line2.start.x and line1.end.y == line2.start.y and line1.end.z == line2.start.z:
+                    duplicates.append(j)
+
+    duplicates = list(set(duplicates))
+    duplicates.sort()
+    duplicates = duplicates[::-1]
+
+    #Remove duplicates
+    for j in range(len(duplicates)):
+        del lines[duplicates[j]]
+
+    return lines
 
 def loadHouse():
     house = []
@@ -100,7 +102,7 @@ def loadHouse():
     house.append(Line3D(Point3D(-5, 5, 5), Point3D(0, 8, 5)))
     house.append(Line3D(Point3D(0, 8, 5), Point3D(5, 5, 5)))
     house.append(Line3D(Point3D(0, 8, 5), Point3D(0, 8, -5)))
-	
+
     return house
 
 def loadCar():
@@ -165,7 +167,14 @@ def loadTire():
     
     return tire
 
-	
+
+
+
+# create quite a bit here i think
+
+
+
+
 # Initialize the game engine
 pygame.init()
  
@@ -188,47 +197,93 @@ clock = pygame.time.Clock()
 start = Point(0.0,0.0)
 end = Point(0.0,0.0)
 linelist = loadHouse()
+CAMERA_X = 0
+CAMERA_Y = 0
+CAMERA_Z = -30
+CAMERA_ROTATE = 0
+CAMERA_VIEW = "perspective"
+CAR_MOVE = -40
+TIRE_ROTATION = 0
+
+
 
 #Loop until the user clicks the close button.
 while not done:
 
-	# This limits the while loop to a max of 100 times per second.
-	# Leave this out and we will use all CPU we can.
+    # This limits the while loop to a max of 100 times per second.
+    # Leave this out and we will use all CPU we can.
     clock.tick(100)
 
-	# Clear the screen and set the screen background
+    # Clear the screen and set the screen background
     screen.fill(BLACK)
 
-	#Controller Code#
-	#####################################################################
+    #Controller Code#
+    #####################################################################
 
     for event in pygame.event.get():
-	    if event.type == pygame.QUIT: # If user clicked close
-		    done=True
+        if event.type == pygame.QUIT: # If user clicked close
+            done=True
             
     
     pressed = pygame.key.get_pressed()
     
     if pressed[pygame.K_w]:
         print("w is pressed")
+        CAMERA_X -= math.sin(math.radians(CAMERA_ROTATE))
+        CAMERA_Z += math.cos(math.radians(CAMERA_ROTATE))
+
+
     if pressed[pygame.K_a]:
         print("a is pressed")
+        CAMERA_X -= math.sin(math.radians(CAMERA_ROTATE - 90))
+        CAMERA_Z += math.cos(math.radians(CAMERA_ROTATE - 90))
+
+
     if pressed[pygame.K_s]:
         print("s is pressed")
+        CAMERA_X += math.sin(math.radians(CAMERA_ROTATE))
+        CAMERA_Z -= math.cos(math.radians(CAMERA_ROTATE))
+
     if pressed[pygame.K_d]:
         print("d is pressed")
+        CAMERA_X -= math.sin(math.radians(CAMERA_ROTATE + 90))
+        CAMERA_Z += math.cos(math.radians(CAMERA_ROTATE + 90))
+
+    if pressed[pygame.K_e]:
+        print("e is pressed")
+        CAMERA_ROTATE += 1
+
+    if pressed[pygame.K_q]:
+        print("q is pressed")
+        CAMERA_ROTATE -= 1
 
 
+    if pressed[pygame.K_r]:
+        print("r is pressed")
+        CAMERA_Y -= 1
 
-	#Viewer Code#
-	#####################################################################
+    if pressed[pygame.K_f]:
+        print("f is pressed")
+        CAMERA_Y += 1
+
+    if pressed[pygame.K_h]:
+        print("h is pressed")
+        CAMERA_X = 0
+        CAMERA_Y = 0
+        CAMERA_Z = -30
+        CAMERA_ROTATE = 0
+        CAR_MOVE = -40
+        TIRE_ROTATION = 0
+
+    #Viewer Code#
+    #####################################################################
 
     for s in linelist:
-		#BOGUS DRAWING PARAMETERS SO YOU CAN SEE THE HOUSE WHEN YOU START UP
-	    pygame.draw.line(screen, BLUE, (20*s.start.x+200, -20*s.start.y+200), (20*s.end.x+200, -20*s.end.y+200))
+        #BOGUS DRAWING PARAMETERS SO YOU CAN SEE THE HOUSE WHEN YOU START UP
+        pygame.draw.line(screen, BLUE, (20*s.start.x+200, -20*s.start.y+200), (20*s.end.x+200, -20*s.end.y+200))
 
-	# Go ahead and update the screen with what we've drawn.
-	# This MUST happen after all the other drawing commands.
+    # Go ahead and update the screen with what we've drawn.
+    # This MUST happen after all the other drawing commands.
     pygame.display.flip()
  
 # Be IDLE friendly
